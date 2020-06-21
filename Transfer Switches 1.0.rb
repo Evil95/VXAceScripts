d58975ee21fd1eba5cdddad42fa133fbff53239b
Evil95 <45834434+Evil95@users.noreply.github.com>
2020-06-21 19:41:30 +0200

    Update Transfer Switches 1.0.rb

--- Transfer Switches 1.0.rb
+++ Transfer Switches 1.0.rb
@@ -6,18 +6,18 @@
 #==============================================================================
 class Scene_Map < Scene_Base
   alias :switches_transfer :pre_transfer
-	def pre_transfer
-    switches_transfer
-    # Hier die Switches für die ganzen Mobs eintrage
-    $game_switches[140] = true
-    $game_switches[141] = true
-    $game_switches[142] = true
-    $game_switches[143] = true
-    $game_switches[144] = true
-    $game_switches[145] = true
-    $game_switches[146] = true
-    $game_switches[147] = true
-    $game_switches[151] = true
-    $game_switches[152] = true
-	end
+  def pre_transfer
+  switches_transfer
+  # Hier die Switches für die ganzen Mobs eintrage
+  $game_switches[140] = true
+  $game_switches[141] = true
+  $game_switches[142] = true
+  $game_switches[143] = true
+  $game_switches[144] = true
+  $game_switches[145] = true
+  $game_switches[146] = true
+  $game_switches[147] = true
+  $game_switches[151] = true
+  $game_switches[152] = true
+  end
 end
