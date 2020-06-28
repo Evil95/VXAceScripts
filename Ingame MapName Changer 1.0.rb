a2f718dcbd9246da917b1682d1d8ae5cc5ca20d0
Evil95 <45834434+Evil95@users.noreply.github.com>
2020-06-28 14:52:09 +0200

    Add files via upload

---
+++ Ingame MapName Changer 1.0.rb
@@ -0,0 +1,38 @@
+#==============================================================================
+# Ingame MapName Changer
+# -----------------------------------------------------------------------------
+# Version 1.0 (28.06.2020)
+# Von Evil95
+#==============================================================================
+# How to:
+# Right before a Teleport-Call insert a script with the following text:
+# IMNC::NEWNAME = "New Mapname"
+# 
+# In the event contents list is should look like this:
+#
+# Script: IMNC::NEWNAME = "New Mapname"
+# Control Switches: [0001:Mapname_Switch] = ON
+# Transfer Player:[001:MAP001] (001,001)
+# Control Switches: [0001:Mapname_Switch] = OFF
+#
+#==============================================================================
+module IMNC
+  # Switch ID to turn the new Mapname on and off
+  SID = 1
+  # alternative name
+  NEWNAME = ""
+end
+
+class Window_MapName < Window_Base
+  def refresh
+    contents.clear
+    unless $game_map.display_name.empty?
+      draw_background(contents.rect)
+      if $game_switches[IMNC::SID] == false
+        draw_text(contents.rect, $game_map.display_name, 1)
+      else
+        draw_text(contents.rect, IMNC::NEWNAME, 1)
+      end
+    end
+  end
+end
