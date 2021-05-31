7fc1b78ea1fbc5aab73b99133cb31c93a94a32f8
Evil95 <45834434+Evil95@users.noreply.github.com>
2021-05-31 16:55:47 +0200

    Ingame MapName Changer 1.2.rb

--- Ingame MapName Changer 1.1.rb
+++ Ingame MapName Changer 1.1.rb
@@ -1,19 +1,31 @@
 #==============================================================================
 # Ingame MapName Changer
 # -----------------------------------------------------------------------------
-# Version 1.1 (29.06.2020)
+# Version 1.2 (30.05.2021)
 # Von Evil95
 #==============================================================================
-# IMPORTANT: 
-# Not compatible with Yanflys Ace Save Engine. Original Mapname is still shown.
+#
 # -----------------------------------------------------------------------------
-# HOW TO:
+# Changelog:
+#
+# Version 1.0
+#  - Initial Release
+# Version 1.1
+#  - Added the uses of a game variable to store the mapname
+#  - Bugfix
+# Version 1.1.1
+#  - Bugfix
+# Version 1.2
+#  - Added compatible with Yanfly's Save Engine
+#
+# -----------------------------------------------------------------------------
+# How to:
+#
 # Right before a Teleport-Call insert a script with the following text:
-# $game_variables[MNC::VID] = "New Mapname"
+# $game_variables[IMNC::VID] = "New Mapname"
 # 
 # In the event contents list it should look like this:
-#
-# Script: $game_variables[MNC::VID] = "New Mapname"
+# Script: $game_variables[IMNC::VID] = "New Mapname"
 # Control Switches: [0001:Mapname_Switch] = ON
 # Transfer Player:[001:MAP001] (001,001)
 # Control Switches: [0001:Mapname_Switch] = OFF
@@ -25,6 +37,7 @@ module IMNC
   # Variable ID to store the new name
   VID = 1
 end
+
 class Window_MapName < Window_Base
   def refresh
     contents.clear
@@ -35,6 +48,16 @@ class Window_MapName < Window_Base
       else
         draw_text(contents.rect, $game_variables[IMNC::VID], 1)
       end
+    end
+  end
+end
+
+class Game_Map
+  def display_name
+    if $game_switches[IMNC::SID] == false
+      @map.display_name
+    else
+      @map.display_name = $game_variables[IMNC::VID]
     end
   end
 end
